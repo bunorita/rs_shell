6edc6c4b1a27e1bf8592258c9a32930e6111edb9
Buntaro Orita <buntaro.orita@gmail.com>
2023-04-05 16:22:29 +0900

    execute single command entered from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{io::stdin, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    let cmd = input.trim(); // remove a trailing newline
+    Command::new(cmd).spawn().unwrap();
+    println!("'{}' executed", cmd)
 }
